936258ee07750beb1977f070e1f4c8185333712d
Manali Matkar <Manali Matkar>
2016-11-09 11:33:25 -0600

    port timeout fix try

--- server.js
+++ server.js
@@ -28,5 +28,5 @@ var routes = require('./controllers/burgers_controller.js');
 
 app.use('/', routes);
 
-var port = 3000;
-app.listen(process.env.JAWSDB_URL || port);
+var PORT = 3000;
+app.listen(process.env.PORT || PORT);

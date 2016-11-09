6232d60594e549cd9e53fc415ec9956234f7a93c
Manali Matkar <Manali Matkar>
2016-11-09 10:17:10 -0600

    heroku deploy for JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,27 @@
 // Create the connection to the database and export to be used by the O.R.M.
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-	port: 3306,
-	host: 'localhost',
-	user: 'root',
-	password: 'monalisa79',
-	database: 'burgers_db'
-});
 
-connection.connect(function (err) {
-	if (err) {
-		console.error('error connecting: ' + err.stack);
-		return;
-	}
-	console.log('connected as id ' + connection.threadId);
+var connection;
+
+    if (process.env.JAWSBD_URL) {
+        connection = mysql.createConnection(process.env.JAWSBD_URL);
+    } else {
+        mysql.createConnection({
+            port: 3306,
+            host: 'localhost',
+            user: 'root',
+            password: 'monalisa79',
+            database: 'burgers_db'
+        });
+
+    }
+
+connection.connect(function(err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+    console.log('connected as id ' + connection.threadId);
 });
 
 module.exports = connection;

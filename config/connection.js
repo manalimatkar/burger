767240c11c4dc387840276321d10b46c64bb14c1
Manali Matkar <Manali Matkar>
2016-11-09 10:49:45 -0600

    heroku deploy fix for JawsDB

--- config/connection.js
+++ config/connection.js
@@ -3,25 +3,17 @@ var mysql = require('mysql');
 
 var connection;
 
-    if (process.env.JAWSBD_URL) {
-        connection = mysql.createConnection(process.env.JAWSBD_URL);
-    } else {
-        mysql.createConnection({
-            port: 3306,
-            host: 'localhost',
-            user: 'root',
-            password: 'monalisa79',
-            database: 'burgers_db'
-        });
+if (process.env.JAWSBD_URL) {
+    connection = mysql.createConnection(process.env.JAWSBD_URL);
+} else {
+    mysql.createConnection({
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: 'monalisa79',
+        database: 'burgers_db'
+    });
+};
 
-    }
-
-connection.connect(function(err) {
-    if (err) {
-        console.error('error connecting: ' + err.stack);
-        return;
-    }
-    console.log('connected as id ' + connection.threadId);
-});
-
+connection.connect();
 module.exports = connection;
